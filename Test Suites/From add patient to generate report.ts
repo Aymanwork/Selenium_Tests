3135204aed47345bbb2db8856da03671b69644aa
Selenium <Selenium@Tests>
2023-09-19 13:50:23 +0100

    Test done!

---
+++ Test Suites/From add patient to generate report.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>From add patient to generate report</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>60</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>804bc1cb-29cd-400f-be56-ed1c8156150e</testSuiteGuid>
+   <testCaseLink>
+      <guid>af340691-f4f8-4d0d-88d4-f0d50c7acdc0</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/From add patient to generate report</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
