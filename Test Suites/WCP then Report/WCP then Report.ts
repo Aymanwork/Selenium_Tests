b4f138e0bd6d6c4cbf84b1952cbc2bfd3f14b30f
Selenium <Selenium@Tests>
2023-10-02 10:12:00 +0100

    Test Done!

---
+++ Test Suites/WCP then Report/WCP then Report.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>WCP then Report</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>0</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>2104825e-7d79-4acf-9fbf-5e11f278af05</testSuiteGuid>
+   <testCaseLink>
+      <guid>de40ff10-1209-439d-8dd7-1db54822ca45</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/WCP then Report/WCP then Report/WCP then Report</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
